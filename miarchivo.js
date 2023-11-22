69704309a0327e1dc1343f72388ed9f6bb8dbf69
Cecilia Mary <ceciliamary78@gmail.com>
2023-11-21 22:29:32 -0300

    carrito de compras corregido pero sin funcionar botones suma, resta y total

--- miarchivo.js
+++ miarchivo.js
@@ -60,11 +60,12 @@ const btnVerCarritoCompras = document.getElementById("btnVerCarritoCompras")
 const carritoTable = document.getElementById("carrito")
 
 btnVerCarritoCompras.addEventListener("click", () => {
-  dibujarCarrito()
+  //dibujarCarrito()
   if(carritoTable.style.display === "block"){
     carritoTable.style.display = "none"
   } else{
     carritoTable.style.display = "block"
+    dibujarCarrito()
   }
 })
 
@@ -72,7 +73,7 @@ const comprarProducto = (idProducto) => {
 
     const producto = productosDisponibles.find((producto) => producto.id === idProducto)
 
-    const { nombre, precio, imagen, id, cantidad } = producto
+    const { nombre, precio, imagen, id } = producto
 
     const productoCarrito = carrito.find((producto) => producto.id === idProducto)
 
@@ -84,8 +85,8 @@ const comprarProducto = (idProducto) => {
             imagen: imagen,
             cantidad: 1
         }
-
-    carrito.push(nuevoProductoCarrito)
+    //carrito.push(nuevoProductoCarrito)
+    
     sessionStorage.setItem("carrito", JSON.stringify(carrito) )
     }else{
         const indexProductoCarrito = carrito.findIndex((producto) => producto.id === idProducto)
@@ -146,18 +147,20 @@ function seguirComprando(){
   }
 
 const dibujarCarrito = () => {
-  carrito.forEach(producto =>{
+  listaCarrito.innerHTML = ''
+  carrito.forEach(producto => {
     const {img, nombre, cantidad, precio, id} = producto
 
   let body = document.createElement("tr")
 
 body.className = "producto === carrito"
+
 body.innerHTML = `
 <th><img id="fotoProductoCarrito" src= "${img}" class="fotoProductoCarrito"</th>
 <td>${nombre}</td>
-<td>"${cantidad}"</td>
-<td>"${precio/cantidad}"</td>
-<td>"${precio}"</td>
+<td>${cantidad}</td>
+<td>${precio / cantidad}</td>
+<td>${precio}</td>
 <td>
 <button id="+${id}">+</button>
 <button id="-${id}">-</button>
@@ -168,33 +171,35 @@ listaCarrito.append(body)
 const btnAgregar = document.getElementById(`+${id}`)
 const btnRestar = document.getElementById(`-${id}`)
 
-btnAgregar.addEventListener("click", () => console.log(id))
-btnRestar.addEventListener("click", () => console.log(id))
+btnAgregar.addEventListener("click", () => aumentarCantidad(id))
+btnRestar.addEventListener("click", () => restarCantidad(id))
  });
 
 dibujarFooter()
 }
 
-const dibujarFooter = () =>{
+const dibujarFooter = () => {
   if(carrito.length >0){
     footCarrito.innerHTML = ""
+
     let footer = document.createElement("tr")
 
     footer.innerHTML = `
-    <th><b>Totales:</th>
+    <th><b>Totales:</b></th>
     <td></td>
     <td>${generarTotales().cantidadTotal}</td>
     <td></td>
     <td>${generarTotales().costoTotal}</td>
     `
+
     footCarrito.append(footer)
   }else{
     footCarrito.innerHTML = "<h3>No hay productos en el carrito</h3>"
   }
 }
 
-const generarTotales = () =>{
-  const costoTotal = carrito.reduce((acumulador, {precio}) => total + precio, 0)
+const generarTotales = () => {
+  const costoTotal = carrito.reduce((total, {precio}) => total + precio, 0)
   const cantidadTotal = carrito.reduce((total, {cantidad})=>
   total + cantidad, 0)
   return {
@@ -202,6 +207,7 @@ const generarTotales = () =>{
     cantidadTotal: cantidadTotal
   }
 }
+
 const aumentarCantidad = (id) => {
   const indexProductoCarrito = carrito.findIndex((producto) => producto.id === id)
   const precio = carrito[indexProductoCarrito].precio / carrito[indexProductoCarrito].cantidad
@@ -210,7 +216,7 @@ const aumentarCantidad = (id) => {
   carrito[indexProductoCarrito].precio = precio*carrito[indexProductoCarrito].cantidad
 
   sessionStorage.setItem("carrito", JSON.stringify(carrito))
-  dibujarCarrito()
+ dibujarCarrito()
 
 }
 
@@ -249,8 +255,11 @@ btnBuscarProducto.addEventListener("click", () => {
       title: `Detalles del producto`,
       html: `
       Nombre: ${encontrado.nombre}
+      <br>
       Precio: $${encontrado.precio}
+      <br>
       Cantidad: ${encontrado.cantidad}
+      <br>
       Categoria: ${encontrado.categoria}
       `,
       });
